b974de48986790fea67957e3cdb8df147d749174
CHOI <tlacjd665@gmail.com>
2020-08-30 23:05:56 +0900

    api호출 하는 방식 json-server로 개발서버 만들어 axios로 호출되도록 수정

--- redux-middleware/src/api/posts.js
+++ redux-middleware/src/api/posts.js
@@ -1,31 +1,11 @@
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
-
-//{id, title, body}
-
-const posts = [
-    {
-        id: 1,
-        title: 'redux thunk api tutorial',
-        body: '리덕스 미들웨어 직접 만들어 보기'
-    },
-    {
-        id: 2,
-        title: 'redux thunk api tutorial2',
-        body: '리덕스 미들웨어 직접 만들어 보기2'
-    },
-    {
-        id: 3,
-        title: 'redux thunk api tutorial3',
-        body: '리덕스 미들웨어 직접 만들어 보기3'
-    }
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await axios.get('/posts');
+    return response.data;
 };
 
 export const getPostById = async (id) => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
+    const response = await axios.get(`/posts/${id}`);
+    return response.data;
 };
