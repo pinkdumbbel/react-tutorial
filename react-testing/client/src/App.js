9f50054117a708e9666b7fadc96751f3401d3561
CHOI <tlacjd665@gmail.com>
2022-01-24 20:18:45 +0900

    react testing

--- react-testing/client/src/App.js
+++ react-testing/client/src/App.js
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import { OrderContextProvider } from './contexts/OrderContext';
 import OrderPage from './pages/OrderPage/OrderPage';
+import SummaryPage from './pages/SummaryPage/SummaryPage';
+import CompletePage from './pages/CompletePage/CompletePage';
 function App() {
+  const [step, setStep] = useState(0);
   return (
     <div style={{ padding: '4rem' }}>
       <OrderContextProvider>
-        <OrderPage />
+        {step === 0 && <OrderPage setStep={setStep} />}
+        {step === 1 && <SummaryPage setStep={setStep} />}
+        {step === 2 && <CompletePage setStep={setStep} />}
       </OrderContextProvider>
     </div>
   );
--- react-testing/client/src/App.test.js
+++ react-testing/client/src/App.test.js
@@ -1,4 +1,30 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('', () => {});
+test('주문완료', async () => {
+  render(<App />);
+
+  const americaInput = await screen.findByRole('spinbutton', {
+    name: 'America',
+  });
+
+  userEvent.clear(americaInput);
+  userEvent.type(americaInput, '2');
+
+  const englandInput = await screen.findByRole('spinbutton', {
+    name: 'England',
+  });
+  userEvent.clear(englandInput);
+  userEvent.type(englandInput, '3');
+
+  const insuranceCheckBox = await screen.findByRole('checkbox', {
+    name: 'Insurance',
+  });
+  userEvent.click(insuranceCheckBox);
+
+  const orderButton = screen.getByRole('button', {
+    name: '주문하기',
+  });
+  userEvent.click(orderButton);
+});
